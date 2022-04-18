61c01a0897a8255b8d5e7811d4bc4124ddcf78f4
Joshua Smith <joshua.smith@generalassemb.ly>
2022-04-18 12:18:22 -0500

    pre-review lesson code

--- server.js
+++ server.js
@@ -28,8 +28,14 @@ const products = require('./models/product_model')
 
 // first middleware - middleware executes for every request - 
 // express.static helps express find where certain files are located
+
 app.use(express.static('public'))
 
+// body-parser middleware -> intercept the data from our post request
+// take all of the data content and create an object - req.params 
+// request body -> data - parsed by the middleware
+
+app.use(express.urlencoded({ extended: false }))
 
 
 // CONTROLLER 
@@ -45,6 +51,12 @@ app.use(express.static('public'))
     Note: A response method call is required for every request otherwise the server will "hang" and timeout after 30-60 seconds
 */
 
+// Product "new" route - GET - serve form for creating a new product
+
+app.get('/products/new', (req, res) => {
+    res.render('new.ejs')
+})
+
 // Products "show" route - GET - one product 
 
 // app.get('/products/all', (req, res) => {
@@ -56,9 +68,9 @@ app.get('/products/:id/', (req, res) => {
     let productId = req.params.id
     console.log(' hitting show route')
     // res.send(products[productId])
-    const context = { 
-        oneProduct: products[productId], 
-        message: 'I am the show route' 
+    const context = {
+        oneProduct: products[productId],
+        message: 'I am the show route'
     }
     res.render('show.ejs', context)
 })
@@ -73,10 +85,23 @@ app.get('/products', (req, res) => {
     res.render('index', context)
 })
 
+
 // "Home" route
 app.get('/', (request, response) => response.send('Welcome to Sell-it-UP!'))
 
 
+// "create" route - POST requests -> request body (new product data)
+app.post('/products', (req, res) => {
+    // Stub out the post route 
+    // send a request and verify the request is received 
+    // if we send a post request - body parser intercepts data -> req.body
+    console.log('request body', req.body)
+    products.unshift(req.body)
+    // request object 
+
+    // res.send(req.body)
+    res.redirect('/products')
+})
 
 
 /* 
